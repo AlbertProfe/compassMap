2611a565e7fe832ce6b48620fddad4667bb724d4
albertprofe <albertprofe@gmail.com>
2026-09-21 12:00:03 +0200

    feat: roadmap class now is a entity

--- compassMap/src/main/java/com/example/demo/RoadMap.java
+++ compassMap/src/main/java/com/example/demo/RoadMap.java
@@ -1,8 +1,16 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class RoadMap {
 
     //fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private String description;
